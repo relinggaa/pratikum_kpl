37e85bc18997227dbd251e70e19b60d5e97dcb51
relinggaa <linggayorfel@gmail.com>
2025-03-02 16:56:30 +0700

    mengerjakan modul 3

---
+++ Modul2/tpmodul2_103022300107/tpmodul2_103022300107/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Masukkan huruf : ");
+        string huruf = Console.ReadLine();
+        if (huruf == "A" || huruf == "I" || huruf == "U" || huruf == "E" || huruf == "O" || huruf == "a" || huruf == "i" || huruf == "u" || huruf == "e" || huruf == "o")
+        {
+            Console.WriteLine("Huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine("Huruf konsonan");
+        }
+        int[] angkaGenap = { 2, 4, 6, 8, 10 };
+        for (int i = 0; i < angkaGenap.Length; i++)
+        {
+            Console.WriteLine("Angka genap " + (i + 1) + " : " + angkaGenap[i]);
+        }
+
+
+
+    }
+}
+
---
+++ Modul3/Project1/WinFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+            string userInput = textBox1.Text;
+
+            
+            string message = "Halo " + userInput;
+
+           
+            MessageBox.Show(message, "User Input", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
